937b205602ebe9429660539077645df27a3a5e4d
hitesh211 <85873058+hitesh211@users.noreply.github.com>
2021-08-10 15:41:51 +0530

    Create db.js

--- server/db.js
+++ server/db.js
@@ -3,8 +3,8 @@ const Pool = require('pg').Pool;
 const pool = new Pool({
     user:'postgres',
     password:'962239',
-    host:'13.234.117.77',
-    port: 5000,
+    host:'localhost',
+    port: 5432,
     database:'todo'
 })
 
