564eed373fb796cf84dc6db2c0df6ddc1e82af69
hitesh211 <85873058+hitesh211@users.noreply.github.com>
2021-08-10 14:46:14 +0530

    Update server.js

--- server/server.js
+++ server/server.js
@@ -82,6 +82,6 @@ app.delete("/todos/:id",async(req,res)=>{
 })
 
 
-app.listen(PORT, ()=>{
-    console.log(`server running at port: ${PORT}`);
+app.listen(5000, ()=>{
+    console.log(`server running at port: ${5000}`);
 })
