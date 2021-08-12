a7c22ca6790f055954855595956764b2e2b66151
hitesh211 <85873058+hitesh211@users.noreply.github.com>
2021-08-12 22:27:04 +0530

    Update db.js

--- server/db.js
+++ server/db.js
@@ -3,7 +3,7 @@ const Pool = require('pg').Pool;
 const pool = new Pool({
     user:'postgres',
     password:'postgre12345',
-    host:'database-1.cqf96d2qobjy.ap-south-1.rds.amazonaws.com',
+    host:'postgre.cqf96d2qobjy.ap-south-1.rds.amazonaws.com',
     port: 5432,
     database:'todo'
 })
