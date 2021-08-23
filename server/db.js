978e1e23363721c62cf136bbbb7686d5ba0cfb00
hitesh211 <85873058+hitesh211@users.noreply.github.com>
2021-08-23 20:23:32 +0530

    change ip address

--- server/db.js
+++ server/db.js
@@ -3,7 +3,7 @@ const Pool = require('pg').Pool;
 const pool = new Pool({
     user:'postgres',
     password:'postgre12345',
-    host:'localhost',
+    host:'15.207.254.138',
     port: 5432,
     database:'todo'
 })
